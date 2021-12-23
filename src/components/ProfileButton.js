7160611dc6a6de09f1daef89efa8c45c3c22cb77
Tim <timofeyobraztsov@gmail.com>
2021-12-23 14:26:33 -0800

    login signup

--- src/components/ProfileButton.js
+++ src/components/ProfileButton.js
@@ -1,9 +1,17 @@
 import cs from './ProfileButton.module.css';
+import { useState } from 'react';
+import Tab from './Tab';
 const ProfileButton = (props) => {
+	const [ logged, setLogged ] = useState(false);
+
 	return (
 		<div className={cs.totalContainer}>
-			<img src="./blankprofile.png" alt="PFP" className={cs.pfp} />
-			<span className={cs.fullName}>Timofey Obraztsov</span>
+			{/* <img src="./blankprofile.png" alt="PFP" className={cs.pfp} /> */}
+			{logged ? <span className={cs.fullName}>Timofey Obraztsov</span> : <>
+			<Tab link="/login">Login</Tab>
+			 <Tab link="/signup">Signup</Tab>
+			 </>
+}
 		</div>
 	);
 };
